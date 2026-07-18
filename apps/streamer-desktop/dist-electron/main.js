d3caa25e625e2dcea32bc80af731abe2556e468c
Krishan K Yadav <aniandp@gmail.com>
2026-07-19 02:41:30 +0530

    fixes#4

--- apps/streamer-desktop/dist-electron/main.js
+++ apps/streamer-desktop/dist-electron/main.js
@@ -3,7 +3,7 @@ import "node:path";
 import "node:url";
 import "node:child_process";
 import "node:fs";
-import { M, R, V } from "./main-B3NNwtra.js";
+import { M, R, V } from "./main-BkYNEyup.js";
 export {
   M as MAIN_DIST,
   R as RENDERER_DIST,
